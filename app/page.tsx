e8fc9f0473826e3a8b016ac7859e4d6ca7974116
knhn1004 <49494541+knhn1004@users.noreply.github.com>
2025-09-24 23:01:17 -0700

    Refactor Supabase client creation to be asynchronous
    - Updated createClient function to await cookie store retrieval.
    - Modified page component to use the updated asynchronous createClient function.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { createClient } from '@/utils/supabase/server';
 
 export default async function Page() {
-	const supabase = createClient();
+	const supabase = await createClient();
 
 	return (
 		<h1>CS 160 Online Banking</h1>
--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -5,18 +5,19 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 
-export const createClient = () => {
-	return createServerClient(supabaseUrl!, supabaseKey!, {
-		cookies: {
-			get(name: string) {
-				return (cookies() as any).get(name)?.value;
-			},
-			set(name: string, value: string, options: CookieOptions) {
-				(cookies() as any).set({ name, value, ...options });
-			},
-			remove(name: string, options: CookieOptions) {
-				(cookies() as any).set({ name, value: '', ...options });
-			},
-		},
-	});
+export const createClient = async () => {
+    const cookieStore = await cookies();
+    return createServerClient(supabaseUrl!, supabaseKey!, {
+        cookies: {
+            get(name: string) {
+                return cookieStore.get(name)?.value;
+            },
+            set(name: string, value: string, options: CookieOptions) {
+                cookieStore.set({ name, value, ...options });
+            },
+            remove(name: string, options: CookieOptions) {
+                cookieStore.set({ name, value: '', ...options });
+            },
+        },
+    });
 };
